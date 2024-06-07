78317f812829d97a929a8f727709627bed0d7c5d
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-06-08 02:52:36 +0300

    chore: added entities

---
+++ src/main/java/com/pustovalov/entities/Match.java
@@ -0,0 +1,34 @@
+package com.pustovalov.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+@Table(name = "MATCHES", uniqueConstraints =
+@UniqueConstraint(columnNames = {"PLAYER_ONE", "PLAYER_TWO"}))
+public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PLAYER_ONE")
+    private Player playerOne;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PLAYER_TWO")
+    private Player playerTwo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "WINNER")
+    private Player winner;
+
+}
---
+++ src/main/java/com/pustovalov/entities/Player.java
@@ -0,0 +1,24 @@
+package com.pustovalov.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+@Table(name = "PLAYERS")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME", unique = true, nullable = false, length = 64)
+    private String name;
+
+}
