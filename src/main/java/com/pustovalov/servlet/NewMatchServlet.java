e049b1d014205336b64a1f571479ba166818a826
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-06-11 03:32:40 +0300

    feat: added GeneralServlet
    The basic servlet for all others in the application, defines a common set of functions
    - parsing parameters from the request body
    - validation of string parameters

--- src/main/java/com/pustovalov/servlet/NewMatchServlet.java
+++ src/main/java/com/pustovalov/servlet/NewMatchServlet.java
@@ -2,14 +2,13 @@ package com.pustovalov.servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
 @WebServlet("/new-match")
-public class NewMatchServlet extends HttpServlet {
+public class NewMatchServlet extends GeneralServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String playerOneName = getParamFromBody(req, "player-one-name");
