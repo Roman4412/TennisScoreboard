738cb8323b4e7df9691f9f37af56d9da24635afe
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-06-11 04:27:04 +0300

    feat: added HibernateUtil
    A class that provides a single instance hibernate SessionFactory

---
+++ src/main/java/com/pustovalov/HibernateUtil.java
@@ -0,0 +1,22 @@
+package com.pustovalov;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public final class HibernateUtil {
+    //TODO make a correct singleton for multithreading
+    private static SessionFactory sessionFactory;
+
+    private HibernateUtil() {
+
+    }
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+}
