8a18c33653ccd6de32b74f9716700ef904640d98
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-06-15 01:59:05 +0300

    fix: fixed servlet logic
    - now the parameters coming in the request body are processed correctly
    - http status is now set

--- src/main/java/com/pustovalov/servlet/NewMatchServlet.java
+++ src/main/java/com/pustovalov/servlet/NewMatchServlet.java
@@ -1,21 +1,29 @@
 package com.pustovalov.servlet;
 
-import com.pustovalov.entity.Match;
+import com.pustovalov.service.CurrentMatchService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.hibernate.query.criteria.JpaCriteriaQuery;
 
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet("/new-match")
-public class NewMatchServlet extends GeneralServlet {
+public class NewMatchServlet extends BaseServlet {
+    CurrentMatchService currentMatchService = new CurrentMatchService();
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String playerOneName = getParamFromBody(req, "player-one-name");
-        String playerTwoName = getParamFromBody(req, "player-two-name");
-        validateStringParams(playerOneName, playerTwoName);
-        saveMatchService.save(requestMatchDto);
+        Map<String, String> params = getParamsFromBody(req);
+        String playerOneName = params.get("player-one-name");
+        String playerTwoName = params.get("player-two-name");
+        //validateStringParams(playerOneName, playerTwoName);
+        currentMatchService.save(playerOneName, playerTwoName);
+        resp.setStatus(HttpServletResponse.SC_SEE_OTHER);
+
+        //TODO редирект на /match-score?uuid=$match_id
+
     }
+
 }
