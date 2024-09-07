c805dbf50c779d9653a252736df0388414f6d6f8
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2024-09-07 18:55:03 +0300

    refactor mappers

--- src/main/java/com/pustovalov/service/mapper/MatchMapper.java
+++ src/main/java/com/pustovalov/service/mapper/MatchMapper.java
@@ -10,7 +10,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper()
+@Mapper
 public interface MatchMapper {
   MatchMapper INSTANCE = Mappers.getMapper(MatchMapper.class);
 
